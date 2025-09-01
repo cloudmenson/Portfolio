9a8fb25be6059bb51bf1e82940814526952fee6a
Danylo <cloudmenson9@gmail.com>
2025-09-01 20:47:38 +0300

    sort soc data

--- shared/data/social-data.ts
+++ shared/data/social-data.ts
@@ -9,20 +9,20 @@ import {
 export const socialData = [
   {
     id: 1,
-    Icon: Github,
-    href: "https://github.com/cloudmenson",
-  },
-  {
-    id: 2,
     Icon: Linkedin,
     href: "https://www.linkedin.com/in/hrytsenko-danylo/",
   },
   {
-    id: 3,
+    id: 2,
     Icon: Mail,
     href: "mailto:hrytsenko.danylo@gmail.com",
   },
-  { id: 4, Icon: Send, label: "Telegram", href: "https://t.me/cloudmenson" },
+  { id: 3, Icon: Send, label: "Telegram", href: "https://t.me/cloudmenson" },
+  {
+    id: 4,
+    Icon: Github,
+    href: "https://github.com/cloudmenson",
+  },
   {
     id: 5,
     Icon: CircleArrowOutUpRight,
