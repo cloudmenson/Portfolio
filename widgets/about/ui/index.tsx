1a969c527d3142df543090d51939be36c227c75d
Danylo <cloudmenson9@gmail.com>
2025-09-01 20:45:11 +0300

    update about

--- widgets/about/ui/index.tsx
+++ widgets/about/ui/index.tsx
@@ -91,7 +91,7 @@ export const AboutSection = () => {
               className="inline-flex mr-1.5 w-5 h-5 sm:w-6 sm:h-6 align-middle"
             />
             Later, I joined{" "}
-            <span className="italic text-yellow-400">NDA project (gambling)</span>,
+            <span className="italic text-yellow-400">NDA (gambling)</span>,
             contributing to the development of a large-scale multi-module
             platform. There I worked closely with designers, backend developers,
             and product managers to deliver high-quality features, integrate
