cb43b5ee5c3e9121c000508a303a782fcf726055
Danylo <cloudmenson9@gmail.com>
2025-09-19 14:57:14 +0300

    added project

--- shared/assets/index.ts
+++ shared/assets/index.ts
@@ -4,18 +4,21 @@ import Code from "./icons/code.svg?react";
 import ConfettiEmoji from "./lottie/confetti-emoji.json";
 import DesignerPortfolio from "./images/projects/designer-portfolio.png";
 import FireEmoji from "./lottie/fire.json";
+import ImmersiveDesign from "./images/projects/immersive-design.png";
 import MyPortfolio from "./images/projects/my-portfolio.png";
 import RocketEmoji from "./lottie/rocket-emoji.json";
 import SparklesEmoji from "./lottie/sparkles-emoji.json";
 import StarStrikeEmoji from "./lottie/star-strike-emoji.json";
 import SunglassEmoji from "./lottie/sunglass-emoji.json";
 import WaveEmoji from "./lottie/wave-emoji.json";
+
 export const Icons = { Code };
 
 export const Img = {
   AboutAvatar,
   Adminpanel,
   DesignerPortfolio,
+  ImmersiveDesign,
   MyPortfolio,
 };
 
